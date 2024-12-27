252ff8210325974b2c1d619f73a452c69a90b8af
雪恕 <huxy22022@shanghaitech.edu.cn>
2024-12-27 14:41:02 +0800

    update

--- src/development/ADMM.m
+++ src/development/ADMM.m
@@ -1,5 +1,6 @@
-function x = ADMM(A,l,u, q, P, x0, y0, z0,rho,prim_tol,dual_tol,max_iter)
+function x = ADMM(A1,A2,b1,b2, q, P, x0, y0, z0,rho,prim_tol,dual_tol,max_iter)
     % Import necessary utility functions
+    [A,l,u] = standard_constraints(A2,-b2,A1,-b1);
     sigma = 1e-6;
     alpha = 1.6;
     x = x0;
@@ -7,19 +8,23 @@ function x = ADMM(A,l,u, q, P, x0, y0, z0,rho,prim_tol,dual_tol,max_iter)
     z = z0;
     n = size(P, 1);
     m = size(A, 1);
-    function [x_tilde, v] = solve_linear_sys()
-        M = [P + sigma * eye(n), A';
-            A, -1 / rho * eye(m)];
-        b = [sigma * x - q; z - 1 / rho * y];
-        xv = lsqminnorm(M,b);
-        % xv = M \ b;  % Solve the linear system
-        x_tilde = xv(1:n);
-        v = xv(n + 1:end);
-    end
+    % function [x_tilde, v] = solve_linear_sys()
+    %     M = [P + sigma * eye(n), A';
+    %         A, -1 / rho * eye(m)];
+    %     b = [sigma * x - q; z - 1 / rho * y];
+    %     xv = lsqminnorm(M,b);
+    %     % xv = M \ b;  % Solve the linear system
+    %     x_tilde = xv(1:n);
+    %     v = xv(n + 1:end);
+    % end
 
     for i = 1: max_iter
-        [x_tilde, v] = solve_linear_sys();
-        z_hat = z + 1 / rho * (v - y);
+        % [x_tilde, v] = solve_linear_sys();
+        % z_hat = z + 1 / rho * (v - y);
+        M = P + sigma * eye(n) + rho * (A'* A);
+        b =  sigma* x - q + A'*(rho*z - y );
+        x_tilde = conjgrad(M,b);
+        z_hat = A*x_tilde;
         x_next = alpha * x_tilde + (1 - alpha) * x;
         z_next = Pc(alpha * z_hat + (1 - alpha) * z + 1 / rho * y, l, u);
         y = y + rho * (alpha * z_hat + (1 - alpha) * z - z_next);
